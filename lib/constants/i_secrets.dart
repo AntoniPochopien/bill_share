6a6cf4f0542fab6cb58f1140af7c0bd194e313c0
Antoni Pochopien <pochopienantek@gmail.com>
2025-01-20 19:35:35 +0100

    update

--- lib/constants/i_secrets.dart
+++ lib/constants/i_secrets.dart
@@ -1,5 +1,6 @@
 abstract class ISecrets {
   static const String supabaseUrl = 'your_supabase_url';
+  static const String supabasePublicStorageUrl = 'your_supabase_public_storage_url';
   static const String supabaseAnonKey = 'your_supabase_anon_key';
   static const String googleClientIdAndroid = 'your_android_google_client_id';
   static const String googleClientIdIos = 'your_ios_google_client_id';
